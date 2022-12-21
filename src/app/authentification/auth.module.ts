db3481d6614658b4551c063a60e970fc8990d2b7
soulaimaKH <soulaima.kahla@dqlick.com>
2022-12-21 20:56:33 +0100

    auth component +admin component

---
+++ src/app/authentification/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { AuthentificationComponent } from "./authentification.component";
+import { AuthInterceptor } from "./services/auth.interceptor";
+import { AuthService } from "./services/auth.services";
+
+/**
+ * The AuthModule class
+ */
+@NgModule({
+    imports: [
+  
+    ],
+    declarations: [
+        AuthentificationComponent
+    ],
+    providers: [AuthInterceptor,AuthService],
+    entryComponents:[
+        AuthentificationComponent,
+    ],
+})
+export class AuthModule {
+}
