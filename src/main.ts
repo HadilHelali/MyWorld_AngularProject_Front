3252e22ff26c756ba6a3321517b3ad1b51d0e55a
soulaimaKH <soulaima.kahla@dqlick.com>
2023-01-19 12:10:20 +0100

    final

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-/*
-=========================================================
-* BLK Design System Angular - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/blk-design-system-angular
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- */
-
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
